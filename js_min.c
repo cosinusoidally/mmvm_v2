04baec3c3f60d92aa0de40b3a0c832dc4364c8bf
Liam Wilson <cosinusoidally@gmail.com>
2025-12-19 23:51:19 +0000

    more

--- js_min.c
+++ js_min.c
@@ -528,120 +528,6 @@ JSClass global_class = {
 };
 
 static JSBool
-env_setProperty(JSContext *cx, JSObject *obj, jsval id, jsval *vp)
-{
-/* XXX porting may be easy, but these don't seem to supply setenv by default */
-#if !defined XP_BEOS && !defined XP_OS2 && !defined SOLARIS
-    JSString *idstr, *valstr;
-    const char *name, *value;
-    int rv;
-
-    idstr = JS_ValueToString(cx, id);
-    valstr = JS_ValueToString(cx, *vp);
-    if (!idstr || !valstr)
-        return JS_FALSE;
-    name = JS_GetStringBytes(idstr);
-    value = JS_GetStringBytes(valstr);
-#if defined XP_WIN || defined HPUX || defined OSF1 || defined IRIX
-    {
-        char *waste = JS_smprintf("%s=%s", name, value);
-        if (!waste) {
-            JS_ReportOutOfMemory(cx);
-            return JS_FALSE;
-        }
-        rv = putenv(waste);
-#ifdef XP_WIN
-        /*
-         * HPUX9 at least still has the bad old non-copying putenv.
-         *
-         * Per mail from <s.shanmuganathan@digital.com>, OSF1 also has a putenv
-         * that will crash if you pass it an auto char array (so it must place
-         * its argument directly in the char *environ[] array).
-         */
-        free(waste);
-#endif
-    }
-#else
-    rv = setenv(name, value, 1);
-#endif
-    if (rv < 0) {
-        JS_ReportError(cx, "can't set envariable %s to %s", name, value);
-        return JS_FALSE;
-    }
-    *vp = STRING_TO_JSVAL(valstr);
-#endif /* !defined XP_BEOS && !defined XP_OS2 && !defined SOLARIS */
-    return JS_TRUE;
-}
-
-static JSBool
-env_enumerate(JSContext *cx, JSObject *obj)
-{
-    static JSBool reflected;
-    char **evp, *name, *value;
-    JSString *valstr;
-    JSBool ok;
-
-    if (reflected)
-        return JS_TRUE;
-
-    for (evp = (char **)JS_GetPrivate(cx, obj); (name = *evp) != NULL; evp++) {
-        value = strchr(name, '=');
-        if (!value)
-            continue;
-        *value++ = '\0';
-        valstr = JS_NewStringCopyZ(cx, value);
-        if (!valstr) {
-            ok = JS_FALSE;
-        } else {
-            ok = JS_DefineProperty(cx, obj, name, STRING_TO_JSVAL(valstr),
-                                   NULL, NULL, JSPROP_ENUMERATE);
-        }
-        value[-1] = '=';
-        if (!ok)
-            return JS_FALSE;
-    }
-
-    reflected = JS_TRUE;
-    return JS_TRUE;
-}
-
-static JSBool
-env_resolve(JSContext *cx, JSObject *obj, jsval id, uintN flags,
-            JSObject **objp)
-{
-    JSString *idstr, *valstr;
-    const char *name, *value;
-
-    if (flags & JSRESOLVE_ASSIGNING)
-        return JS_TRUE;
-
-    idstr = JS_ValueToString(cx, id);
-    if (!idstr)
-        return JS_FALSE;
-    name = JS_GetStringBytes(idstr);
-    value = getenv(name);
-    if (value) {
-        valstr = JS_NewStringCopyZ(cx, value);
-        if (!valstr)
-            return JS_FALSE;
-        if (!JS_DefineProperty(cx, obj, name, STRING_TO_JSVAL(valstr),
-                               NULL, NULL, JSPROP_ENUMERATE)) {
-            return JS_FALSE;
-        }
-        *objp = obj;
-    }
-    return JS_TRUE;
-}
-
-static JSClass env_class = {
-    "environment", JSCLASS_HAS_PRIVATE | JSCLASS_NEW_RESOLVE,
-    JS_PropertyStub,  JS_PropertyStub,
-    JS_PropertyStub,  env_setProperty,
-    env_enumerate, (JSResolveOp) env_resolve,
-    JS_ConvertStub,   JS_FinalizeStub
-};
-
-static JSBool
 snarf(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
 {
     JSString *str;
@@ -834,10 +720,6 @@ main(int argc, char **argv, char **envp)
     /* Set version only after there is a global object. */
     if (version != JSVERSION_DEFAULT)
         JS_SetVersion(cx, version);
-
-    envobj = JS_DefineObject(cx, glob, "environment", &env_class, NULL, 0);
-    if (!envobj || !JS_SetPrivate(cx, envobj, envp))
-        return 1;
 
     result = ProcessArgs(cx, glob, argv, argc);
 
