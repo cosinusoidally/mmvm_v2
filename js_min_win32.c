26b1b469f1c7ba2cc19c8caaf5c14857b382f236
Liam Wilson <cosinusoidally@gmail.com>
2026-01-15 11:28:33 +0000

    more

--- js_min_win32.c
+++ js_min_win32.c
@@ -6,16 +6,26 @@ HMODULE foo = 0;
 int dlsym(int handle, char* symbol) {
   int d = 0;
   printf("looking up sym %s\n", symbol);
-  if(foo) {
+  if(foo == 0) {
     printf("setup foo\n");
-    foo = GetModuleHandle(0);
+    foo = GetModuleHandle("msvcrt.dll");
+    if(foo == 0) {
+      printf("can't get handle\n");
+    }
   }
+
+  d = GetProcAddress(foo, symbol);
+
   printf("symbol %s address %d\n", symbol, d);
+/*
   if(d == (int)puts) {
     return (int)puts;
   }
   puts("dlsym not impl");
   exit(1);
+*/
+
+  return d;
 }
 
 #include "js_min.c"
