93589c682d56a5dcba22d176586c28915c95b197
Liam Wilson <cosinusoidally@gmail.com>
2026-01-15 11:16:52 +0000

    more

--- js_min_win32.c
+++ js_min_win32.c
@@ -1,4 +1,16 @@
-int dlsym() {
+#include <windows.h>
+
+HMODULE foo = 0;
+
+int dlsym(int handle, char* symbol) {
+  int d = 0;
+  printf("looking up sym %s\n", symbol);
+  if(foo) {
+    printf("setup foo\n");
+  } else {
+    foo = 1;
+    printf("symbol %s address %d\n", symbol, d);
+  }
   puts("dlsym not impl");
   exit(1);
 }
